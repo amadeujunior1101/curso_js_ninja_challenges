8c0e4eeebdf7970dadab9ad25a995e1e146a8262
amadeujunior1101 <amadeujunior00@gmail.com>
2021-02-11 16:57:59 -0300

    pasta js-exemple e challenge-15

---
+++ js-exemple/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public" )));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3333, () => {
+  console.log("App listening on port 3333!");
+});
