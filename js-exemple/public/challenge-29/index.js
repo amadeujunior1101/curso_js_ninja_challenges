a8029cd579c713e9799a5132e9bc5cdba8de3b6d
amadeujunior1101 <amadeujunior00@gmail.com>
2021-02-24 16:46:23 -0300

    feat: resolution challenge-29

---
+++ js-exemple/public/challenge-29/index.js
@@ -0,0 +1,70 @@
+(function (DOM) {
+    "use strict";
+
+    /*
+    Vamos estruturar um pequeno app utilizando módulos.
+    Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
+    A primeira etapa vai ser o cadastro de veículos, de deverá funcionar da
+    seguinte forma:
+    - No início do arquivo, deverá ter as informações da sua empresa - nome e
+    telefone (já vamos ver como isso vai ser feito)
+    - Ao abrir a tela, ainda não teremos carros cadastrados. Então deverá ter
+    um formulário para cadastro do carro, com os seguintes campos:
+      - Imagem do carro (deverá aceitar uma URL)
+      - Marca / Modelo
+      - Ano
+      - Placa
+      - Cor
+      - e um botão "Cadastrar"
+    Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
+    carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
+    aparecer no final da tabela.
+    Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
+    empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
+    Dê um nome para a empresa e um telefone fictício, preechendo essas informações
+    no arquivo company.json que já está criado.
+    Essas informações devem ser adicionadas no HTML via Ajax.
+    Parte técnica:
+    Separe o nosso módulo de DOM criado nas últimas aulas em
+    um arquivo DOM.js.
+    E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
+    que será nomeado de "app".
+    */
+
+    function app() {
+        return {
+            init: function init() {
+                console.log("app init");
+                this.companyInfo();
+                this.initEvents();
+            },
+            initEvents: function initEvents() {
+                DOM('[data-js="form-register"]').on("submit", this.handleSubmit);
+            },
+            handleSubmit: function handleSubmit(e) {
+                e.preventDefault();
+                console.log("submit");
+            },
+            companyInfo: function companyInfo() {
+                const ajax = new XMLHttpRequest();
+                ajax.open("GET", "/challenge-29/company.json", true);
+                ajax.send();
+                ajax.addEventListener("readystatechange", this.getCompanyInfo, false);
+            },
+            getCompanyInfo: function getCompanyInfo() {
+                if (!app().isReady.call(this)) return;
+                const data = JSON.parse(this.responseText);
+
+                let $companyName = DOM('[data-js="company-name"]').get();
+                let $companyPhone = DOM('[data-js="company-phone"]').get();
+
+                $companyName.textContent = data.name;
+                $companyPhone.textContent = data.phone;
+            },
+            isReady: function isReady() {
+                return this.readyState === 4 && this.status === 200;
+            },
+        };
+    }
+    app().init();
+})(window.DOM);
